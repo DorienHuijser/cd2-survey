3003dad85f57e686f1c1781c7a788f1d45fad358
DorienHuijser <dorienhuijser@outlook.com>
2022-04-22 12:08:27 +0200

    start script merging pilot with survey results

--- docs/CD2-vocabularies-merge-pilot-survey.R
+++ docs/CD2-vocabularies-merge-pilot-survey.R
@@ -6,7 +6,7 @@
 # and saves those as .csv in a folder in 
 # data/raw/processed/YYYY-MM-DD_results_combined
 #
-# Last edit 2022-04-21 by Dorien Huijser
+# Last edit 2022-04-22 by Dorien Huijser
 
 ### Prerequisites ###
 # install.packages("data.table")
@@ -18,7 +18,7 @@ library(tidyverse)
 datetoread_pilot <- "2022-04-21"
 datetoread_survey <- "2022-04-07"
 
-cohorts <- c("TRAILS", "GenR", "RADAR", "LCID", "YOUth", "NTR", "all")
+#cohorts <- c("TRAILS", "GenR", "RADAR", "LCID", "YOUth", "NTR")
 
 ### Read in data ####
 filepathsurvey <- file.path(paste0("data/processed/",
@@ -28,56 +28,42 @@ filepathpilot <- file.path(paste0("data/processed/",
                                   datetoread_pilot,
                                   "_pilot_results/"))
 
-listfilessurvey <- list.files(path = filepathsurvey, pattern = "*.csv")
-listfilespilot <- list.files(path = filepathpilot, pattern = "*.csv")
+pilot_all <- fread(file.path(paste0(filepathpilot, "/",
+                                    list.files(path = filepathpilot, 
+                                               pattern = "results_all"))))
+survey_all <- fread(file.path(paste0(filepathsurvey, "/",
+                                    list.files(path = filepathsurvey, 
+                                               pattern = "results_all"))))
 
-# Read in all data files as separate R objects
-for (filenr in 1:length(listfilessurvey)){
-  assign(gsub("^[0-9]+-[0-9]+-[0-9]+_|*.csv", 
-              "", 
-              listfilessurvey[filenr]), 
-         fread(file.path(filepathsurvey, 
-                         listfilessurvey[filenr])))
-  
-  assign(gsub("^[0-9]+-[0-9]+-[0-9]+_|*.csv", 
-              "", 
-              listfilespilot[filenr]), 
-         fread(file.path(filepathpilot, 
-                         listfilespilot[filenr])))
-}
+### Separate cohorts ####
+# results in a list variable containing results per cohort
+pilot_list <- split(pilot_all, pilot_all$Cohort)
+survey_list <- split(survey_all, survey_all$Cohort)
 
-# Put names of files in a character vector
-objectspilot <- character(0)
-objectssurvey <- character(0)
 
-for(cohort in 1:length(cohorts)) { 
-  objectspilot[cohort] <- paste0("pilotresults_", cohorts[cohort])
-  objectssurvey[cohort] <- paste0("surveyresults_",cohorts[cohort])
-}
 
 ### Merge the data from each cohort ###
-# Delete numbers from start of instrument_name
+merged_list <- vector(mode = "list", length = length(pilot_list)) 
 
-
-
-# Probeersels
-objectspilot %>%
-  apply(mutate(Instrument_name = 
-                 str_replace_all(as.character(Instrument_name), 
-                                 "^[0-9]+_", 
-                                 "")))
-      
-# werkt niet
-for(file in objectspilot){
-  file %>%
-    mutate(Instrument_name = str_replace_all(as.character(Instrument_name), 
-                                          "^[0-9]+_", ""))
+for(cohort in 1:length(pilot_list)){
+  # Code copied from: https://stackoverflow.com/questions/27167151/merge-combine-columns-with-same-name-but-incomplete-data
+  merged_list[[cohort]] <- pilot_list[[cohort]] %>%
+    full_join(survey_list[[cohort]], 
+              by = intersect(colnames(pilot_list[[cohort]]), 
+                             colnames(survey_list[[cohort]]))) %>%
+    group_by(Instrument_name) %>%
+    #summarize_all(na.omit) %>%
+    filter(!(Keywords == "") & !is.na(Categories)) %>%
+    arrange(Instrument_name)
 }
 
-results_all$Instrument_name <- str_replace_all(as.character(results_all$Instrument_name), 
-                                               "^[0-9]+_", "")
+# ^ This adds a row for each measure that is scored in both the survey and the
+# pilot and does not distinguish yet between the pilot and survey data -->
+# such distinction should be made, preferably so that each (matching) measure
+# takes up only 1 row instead of sometimes 2.
 
 
+## TO DO FROM HERE ONWARDS ###
 ### Save the merged files in a separate folder ###
 todaysdate <- as.character(Sys.Date())
 results_folder <- file.path(paste0("data/processed/",
@@ -86,35 +72,10 @@ results_folder <- file.path(paste0("data/processed/",
 dir.create(results_folder)
 
 # Write each dataframe to a .csv file
-for(cohort in cohorts){
+for(cohort in merged_list){
   write.csv(get(paste0("results_",cohort)), 
             file.path(paste0(results_folder, 
                              "/", 
                              todaysdate,
                              "_mergedresults_",cohort,".csv")), row.names = TRUE)
 }
-
-
-# OLD
-write.csv(results_TRAILS, file.path(paste0(results_folder, "/", todaysdate, 
-                                           "_mergedresults_TRAILS.csv")), row.names = TRUE)
-write.csv(results_GenR, file.path(paste0(results_folder, "/", todaysdate,
-                                         "_pilotresults_GenR.csv")), row.names = TRUE)
-write.csv(results_RADAR, file.path(paste0(results_folder, "/", todaysdate, 
-                                          "_pilotresults_RADAR.csv")), row.names = TRUE)
-write.csv(results_LCID,file.path(paste0(results_folder, "/", todaysdate,
-                                        "_pilotresults_LCID.csv")), row.names = TRUE)
-write.csv(results_YOUth,file.path(paste0(results_folder, "/", todaysdate,
-                                         "_pilotresults_YOUth.csv")), row.names = TRUE)
-write.csv(results_NTR,file.path(paste0(results_folder, "/", todaysdate, 
-                                       "_pilotresults_NTR.csv")), row.names = TRUE)
-
-# Combine all cohorts in 1 file (delete instrument nrs for easier analysis)
-results_all <- rbind(results_TRAILS,results_GenR, results_RADAR, 
-                     results_LCID, results_YOUth, results_NTR)
-
-results_all$Instrument_name <- str_replace_all(as.character(results_all$Instrument_name), 
-                                               "^[0-9]+_", "")
-
-write.csv(results_all, file.path(paste0(results_folder, "/", todaysdate, 
-                                        "_pilotresults_all.csv")),row.names = TRUE)
